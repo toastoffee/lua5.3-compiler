480e983514195b9c78126d3cf32959d61db5236e
toastoffee <931391442@qq.com>
2024-09-01 11:48:32 +0800

    add block parser

---
+++ src/parser/block_parser.hpp
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : block_parser.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/9/1
+  ******************************************************************************
+  */
+
+
+
+#ifndef LUA5_3_COMPILER_BLOCK_PARSER_HPP
+#define LUA5_3_COMPILER_BLOCK_PARSER_HPP
+
+
+class block_parser {
+
+};
+
+
+#endif //LUA5_3_COMPILER_BLOCK_PARSER_HPP
